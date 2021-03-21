d78952b3f75fa275478e050853eaf7a99a6c8034
Mark Pasricha <markp862@gmail.com>
2021-03-20 20:20:37 -0400

    created basic server with user model

---
+++ app/models/user.js
@@ -0,0 +1,40 @@
+const bcrypt = require("bcrypt"); 
+
+module.exports = (sequelize, DataTypes) =>{
+	const User = sequelize.define("User", {
+		full_name: {
+			type: DataTypes.STRING(50),
+			allowNull: false 
+		}, 
+		first_name:{
+			type: DataTypes.STRING(25), 
+			allowNull: false
+		}, 
+		last_name: {
+			type: DataTypes.STRING(25),
+			allowNull: false
+		}, 
+		email: {
+			type: DataTypes.STRING(25), 
+			allowNull: false, 
+			unique: true, 
+			validate: {
+				isEmail: true
+			}
+		}, 
+		password: {
+			type: DataTypes.STRING(255), 
+			allowNull: false
+		}, 
+		age: DataTypes.INTEGER, 
+		gender: DataTypes.STRING(15)
+	});
+
+	User.prototype.validPassword = (password) =>{
+		return bcrypt.compareSync(password, this.password); 
+	};
+	User.addHook("beforeCreate", (user) =>{
+		user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null)
+	});
+	return User;
+}
---
+++ app/server.js
@@ -0,0 +1,26 @@
+const express = require("express"); 
+const session = require("express-session"); 
+require("dotenv").config();
+
+const passport = require("./config/passport");
+
+const PORT = process.env.PORT || 5000; 
+const db = require("./models");
+
+const app = express(); 
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json()); 
+app.use(express.static("public")); 
+
+app.use(session({ secret: process.env.SECRET_KEY, resave:true, saveUninitialized: true}))
+app.use(passport.initialize()); 
+app.use(passport.session());
+
+// require("./routes/html-routes.js")(app); 
+// require("./routes/api-routes.js")(app); 
+
+db.sequelize.sync().then( () =>{
+	app.listen(PORT, () => {
+		console.log(`Listening on port ${PORT}`);
+	});
+});
