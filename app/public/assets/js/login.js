eebbe96e1559d3f0d124e655d593b645a7dd91c8
Mark Pasricha <markp862@gmail.com>
2021-03-23 21:59:09 -0400

    cleaning up folder before merge

--- app/public/assets/js/login.js
+++ app/public/assets/js/login.js
@@ -25,12 +25,12 @@ $(document).ready(function() {
   
     // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
     function loginUser(email, password) {
-      $.post("/api/login", {
+      $.post("/login", {
         email: email,
         password: password
       })
         .then(function() {
-          window.location.replace("/members");
+          window.location.replace("/authenticated");
           // If there's an error, log the error
         })
         .catch(function(err) {
--- app/server.js
+++ app/server.js
@@ -12,7 +12,7 @@ const db = require("./models");
 const app = express(); 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json()); 
-app.use(express.static("public")); 
+app.use('/public',express.static('public'));
 
 app.engine("handlebars", exphbs({ defaultLayout: "main"})); 
 app.set("view engine", "handlebars"); 
