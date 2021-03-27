812020cae7d1ba9b8e0ecfacb5ea621bf227be47
Mark Pasricha <markp862@gmail.com>
2021-03-27 14:57:47 -0400

    Added cat seed file and db call in get route to pass cats to addme

--- controllers/private-controller.js
+++ controllers/private-controller.js
@@ -8,7 +8,17 @@ router.get("/authenticated", isAuthenticated, (req,res) =>{
 	res.render("authenticated")
 }); 
 router.get("/addnew", isAuthenticated, (req,res) =>{
-	res.render("addnew")
+	db.Categories.findAll({
+		attributes: ["id", "category_name"]
+	}).then( (dbCategories) =>{
+		let categories = dbCategories.map(category => 
+				({id: category.dataValues.id, category: category.dataValues.category_name}));
+		res.render("addnew", {categories}); 
+	});
+}); 
+
+router.post("/addnew", isAuthenticated, (req,res) =>{
+	console.log(req.body)
 }); 
 
 module.exports = router;
---
+++ db/categories-seed.sql
@@ -0,0 +1,7 @@
+INSERT INTO categories(category_name, createdAt, updatedAt) VALUES("T-Shirt", NOW(), NOW()); 
+INSERT INTO categories(category_name, createdAt, updatedAt) VALUES("Jeans", NOW(), NOW()); 
+INSERT INTO categories(category_name, createdAt, updatedAt) VALUES("Dress", NOW(), NOW()); 
+INSERT INTO categories(category_name, createdAt, updatedAt) VALUES("Dress Pants", NOW(), NOW()); 
+INSERT INTO categories(category_name, createdAt, updatedAt) VALUES("Sneakers", NOW(), NOW()); 
+INSERT INTO categories(category_name, createdAt, updatedAt) VALUES("Socks", NOW(), NOW()); 
+INSERT INTO categories(category_name, createdAt, updatedAt) VALUES("Hat", NOW(), NOW()); 
