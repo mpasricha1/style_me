d9bbb16710a10eaadf9651420016907111189025
Your NameEnyaSantiagogit config --global user.namegit config --global user.name Enya <enyasantiago@me.com>
2021-03-28 22:35:46 -0400

    added to landing page

---
+++ config 2/passport.js
@@ -0,0 +1,36 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+var db = require("../models");
+
+passport.use(new LocalStrategy(
+	{
+		usernameField: "email"
+	},
+	(email, password, done) =>{
+		db.User.findOne({
+			where: {
+				email: email
+			}
+		}).then( (dbUser) =>{
+			console.log(dbUser);
+			console.log(password);
+			if(!dbUser || !dbUser.validPassword(password)){
+				return done(null, false, {
+					message: "Invalid username or password."
+				});
+			}
+			return done(null, dbUser);
+		});
+	}	
+));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+module.exports = passport;
