1c07cdf70aed6d0020834a435590f560dceff55a
KevinFloris20 <75303163+KevinFloris20@users.noreply.github.com>
2021-03-29 22:45:19 -0400

    updated code

--- controllers/private-controller.js
+++ controllers/private-controller.js
@@ -54,6 +54,35 @@ const getAllCategories = () => {
 		attributes: ["id", "category_name"]
 	})
 };
+
+//-------------------------------test
+// router.get("/catalog", async (req,res) =>{
+// 	try{
+// 		if(req.session.cat_id){
+// 			var items = await getAllItemsByCategory(req.session.cat_id, req.user.id);
+// 			items = mapItems(items);
+// 		}
+// 		let categories = await getAllCategories(); 
+// 		categories = mapCategories(categories);
+		
+// 		res.render("catalog", {categories: categories, newOutfititems: items} );
+// 	}catch(err){
+// 		if(err) return res.status(500).end();
+// 	}
+// });
+
+// router.post("/catalog", (req, res) =>{
+// 	req.session.cat_id = req.body.id;  
+// 	res.redirect("/catalog")
+// });
+
+// const getAllCategories = () => {
+// 	return db.Categories.findAll({
+// 		attributes: ["id", "category_name"]
+// 	})
+// };
+
+//---------------------------------------test
 const mapCategories = (categories) => {
 	return categories.map(category => 
 	 			({id: category.dataValues.id, category: category.dataValues.category_name}));
@@ -70,3 +99,10 @@ const mapItems = (items) =>{
 };
 
 module.exports = router;
+
+//SELECT outfit_name
+// FROM Catalogs c
+// INNER JOIN catalog_items ci ON c.id = ci.catalog_id
+// INNER JOIN outfits o ON o.Id = ci.outfit_id
+// WHERE c.UserID = 1
+// 	  AND c.id = 1
