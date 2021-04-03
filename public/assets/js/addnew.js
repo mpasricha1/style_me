df9bc977eeb3a8599646d80282979750c05cc590
Mark Pasricha <markp862@gmail.com>
2021-04-02 21:29:08 -0400

    adding key

--- public/assets/js/addnew.js
+++ public/assets/js/addnew.js
@@ -4,16 +4,11 @@ let id;
 
 // Cloudinary widget
 
-var myWidget = cloudinary.config({ 
-    cloud_name: 'dnx8v0ryu', 
-    api_key: "712657743481962", 
-    api_secret: "7utMBxMsMw--1lG-Rec7dp44j9I" 
-  });
-
-myWidget = cloudinary.createUploadWidget(
+var myWidget = cloudinary.createUploadWidget(
   {
     cloudName: "dnx8v0ryu",
-    uploadPreset: "qvqp5qcx"
+    uploadPreset: "qvqp5qcx", 
+    apiKey: "712657743481962"
   },
   (error, result) => {
     if (!error && result && result.event === "success") {
